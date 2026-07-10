052bc2462e82f4eac649ebcf7e37eea010ceb732
Morena <xsvsg4qnt4@privaterelay.appleid.com>
2026-07-10 03:27:48 -0300

    refactor: organizar dashboard con CSS Grid

---
+++ MoneyTrack/frontend/src/components/dashboard/StatCard.jsx
@@ -0,0 +1,22 @@
+import styles from "./StatCard.module.css";
+
+function StatCard({ title, value, icon, color }) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.top}>
+        <span
+          className={styles.icon}
+          style={{ backgroundColor: color }}
+        >
+          <i className={icon}></i>
+        </span>
+
+        <span className={styles.title}>{title}</span>
+      </div>
+
+      <h2 className={styles.value}>{value}</h2>
+    </div>
+  );
+}
+
+export default StatCard;
--- MoneyTrack/frontend/src/pages/Dashboard/Dashboard.jsx
+++ MoneyTrack/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,46 @@
+import styles from "./Dashboard.module.css";
+import StatCard from "../../components/dashboard/StatCard";
+
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className={styles.dashboard}>
+      <h1 className={styles.title}>Bienvenido, More 👋</h1>
+
+      <p className={styles.subtitle}>
+        Controla tus ingresos, gastos y objetivos financieros desde un solo lugar.
+      </p>
+
+      <div className={styles.cardsGrid}>
+  <StatCard
+    title="Saldo Total"
+    value="$185.000"
+    icon="bi bi-wallet2"
+    color="#2563eb"
+  />
+
+  <StatCard
+    title="Ingresos"
+    value="$240.000"
+    icon="bi bi-arrow-down-circle"
+    color="#22c55e"
+  />
+
+  <StatCard
+    title="Gastos"
+    value="$55.000"
+    icon="bi bi-arrow-up-circle"
+    color="#ef4444"
+  />
+
+  <StatCard
+    title="Ahorros"
+    value="$60.000"
+    icon="bi bi-piggy-bank"
+    color="#f59e0b"
+  />
+</div>
+    </div>
+  );
 }
 
 export default Dashboard;
